930dfea6c4c4f48895b41e1d5d4d6686c57e09ac
MsDaria08 <kotovadasha08@gmail.com>
2022-04-29 12:11:24 +0300

    changes hash

---
+++ cmake/projects/factorial/hunter.cmake
@@ -0,0 +1,22 @@
+# !!! DO NOT PLACE HEADER GUARDS HERE !!!
+# made by MsDaria08
+
+include(hunter_add_version)
+include(hunter_cacheable)
+include(hunter_download)
+include(hunter_pick_scheme)
+
+hunter_add_version(
+    PACKAGE_NAME
+    fib
+    VERSION
+    "1.0.1"
+    URL
+    https://github.com/MsDaria08/lab07/archive/refs/tags/v_1.tar.gz
+    SHA1
+    0000a2f992b167145d381fb0d6fbc37584761380
+)
+
+hunter_pick_scheme(DEFAULT url_sha1_cmake)
+hunter_cacheable(factorial)
+hunter_download(PACKAGE_NAME factorial)
